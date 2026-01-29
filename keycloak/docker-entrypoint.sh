a9e15b746a54d0cc250e4e31c41019388462fece
vpoluyaktov <vpoluyaktov@gmail.com>
2026-01-29 11:33:46 -0800

    feat: Rename users and add dedicated opds_user
    - Rename admin -> hub_admin (full access to all services)
    - Rename user -> hub_user (read-only access)
    - Add opds_user (OPDS feed Basic Auth access only)
    - Update env var names: BIBLIO_HUB_ADMIN_PASSWORD, BIBLIO_HUB_USER_PASSWORD, BIBLIO_OPDS_USER_PASSWORD

--- keycloak/docker-entrypoint.sh
+++ keycloak/docker-entrypoint.sh
@@ -8,8 +8,9 @@ set -e
 export BIBLIO_HUB_HOSTNAME="${BIBLIO_HUB_HOSTNAME:-localhost}"
 export BIBLIO_HUB_PORT="${BIBLIO_HUB_PORT:-9900}"
 export OPDS_OIDC_CLIENT_SECRET="${OPDS_OIDC_CLIENT_SECRET:-opds-server-secret-key-2026}"
-export BIBLIO_ADMIN_PASSWORD="${BIBLIO_ADMIN_PASSWORD:-admin}"
-export BIBLIO_USER_PASSWORD="${BIBLIO_USER_PASSWORD:-user}"
+export BIBLIO_HUB_ADMIN_PASSWORD="${BIBLIO_HUB_ADMIN_PASSWORD:-admin}"
+export BIBLIO_HUB_USER_PASSWORD="${BIBLIO_HUB_USER_PASSWORD:-user}"
+export BIBLIO_OPDS_USER_PASSWORD="${BIBLIO_OPDS_USER_PASSWORD:-opds}"
 
 TEMPLATE_FILE="/opt/keycloak/data/import/biblio-realm-template.json"
 OUTPUT_FILE="/opt/keycloak/data/import/biblio-realm.json"
@@ -20,8 +21,9 @@ if [ -f "$TEMPLATE_FILE" ]; then
     sed -e "s/\${BIBLIO_HUB_HOSTNAME}/$BIBLIO_HUB_HOSTNAME/g" \
         -e "s/\${BIBLIO_HUB_PORT}/$BIBLIO_HUB_PORT/g" \
         -e "s/\${OPDS_OIDC_CLIENT_SECRET}/$OPDS_OIDC_CLIENT_SECRET/g" \
-        -e "s/\${BIBLIO_ADMIN_PASSWORD}/$BIBLIO_ADMIN_PASSWORD/g" \
-        -e "s/\${BIBLIO_USER_PASSWORD}/$BIBLIO_USER_PASSWORD/g" \
+        -e "s/\${BIBLIO_HUB_ADMIN_PASSWORD}/$BIBLIO_HUB_ADMIN_PASSWORD/g" \
+        -e "s/\${BIBLIO_HUB_USER_PASSWORD}/$BIBLIO_HUB_USER_PASSWORD/g" \
+        -e "s/\${BIBLIO_OPDS_USER_PASSWORD}/$BIBLIO_OPDS_USER_PASSWORD/g" \
         "$TEMPLATE_FILE" > "$OUTPUT_FILE"
     echo "Realm configuration generated at $OUTPUT_FILE"
 else
