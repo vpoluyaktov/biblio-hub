3cf0ca1e95cffd27c2640f34f1d03950157f6a6e
vpoluyaktov <vpoluyaktov@gmail.com>
2026-03-17 10:27:27 -0700

    Add abb-ia to service monitoring in start_stack.sh

--- scripts/start_stack.sh
+++ scripts/start_stack.sh
@@ -68,7 +68,7 @@ cd "$HUB_DIR"
 docker stack deploy -c stack.yaml --resolve-image always "$STACK_NAME"
 
 # All services to monitor
-ALL_SERVICES="biblio-auth nginx-gateway biblio-catalog abb-tts tts-silero tts-openvoice tts-piper stress-silero"
+ALL_SERVICES="biblio-auth nginx-gateway biblio-catalog abb-tts abb-ia tts-silero tts-openvoice tts-piper stress-silero"
 MAX_WAIT=180  # Maximum wait time in seconds
 POLL_INTERVAL=3
 
